d028876713c70f432a0dc5f0049765d8b5fb2252
tianyu <tianyu@cvte.com>
2016-06-28 14:26:35 +0800

    加入popup的边缘显示策略

--- littlepopup/src/main/java/com/rainfool/littlepopup/LittlePopup.java
+++ littlepopup/src/main/java/com/rainfool/littlepopup/LittlePopup.java
@@ -1,21 +1,28 @@
 package com.rainfool.littlepopup;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
 /**
  * Created by rainfool on 16/6/27.
  */
-public class LittlePopup{
+public class LittlePopup {
 
-    private static final int POPUP_WINDOW_OFFSET_Y = 70;
+    private static final int POPUP_WINDOW_OFFSET_Y = 80;
     private static final int POPUP_WINDOW_PADDING_LEFT_RIGHT = 8 * 2;
+    private static final int POPUP_TRIANGLE_WIDTH_HEIGHT = 24;
+
+    private static int mScreenWidth = 1080;
 
     Context mContext;
 
@@ -39,23 +46,30 @@ public class LittlePopup{
      */
     TextView content;
 
+    ImageView triangle;
+
     View popupView;
 
-    float mWidth,mHeight = 66;
+    float mWidth;
 
-    public LittlePopup(Context context,View attachView) {
+    public LittlePopup(Context context, View attachView) {
         this.mContext = context;
         this.mAttachView = attachView;
         init(context);
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Point screenPoint = new Point();
+        windowManager.getDefaultDisplay().getSize(screenPoint);
+        mScreenWidth = screenPoint.x;
     }
 
     private void init(Context context) {
         LayoutInflater inflater = LayoutInflater.from(context);
-        popupView = inflater.inflate(R.layout.popup_layout,null);
+        popupView = inflater.inflate(R.layout.popup_layout, null);
         content = (TextView) popupView.findViewById(R.id.text);
+        triangle = (ImageView) popupView.findViewById(R.id.indicate);
         popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         popupWindow.setContentView(popupView);
-        this.setLocation(new int[]{0,0});
+        this.setLocation(new int[]{0, 0});
     }
 
     public LittlePopup setLocation(int[] mLocation) {
@@ -67,8 +81,9 @@ public class LittlePopup{
         int[] location = new int[2];
         v.getLocationInWindow(location);
 
-        location[0] -= (mWidth / 2 + 16 - v.getWidth() / 2);
-        location[1] -= mHeight;
+        location[0] -= (mWidth / 2 + POPUP_WINDOW_PADDING_LEFT_RIGHT - v.getWidth() / 2);
+        location[1] -= POPUP_WINDOW_OFFSET_Y;
+
         this.mLocation = location;
         return this;
     }
@@ -79,9 +94,25 @@ public class LittlePopup{
         return this;
     }
 
-    public LittlePopup show(){
+    public LittlePopup show() {
         if (popupWindow != null) {
             Log.d("test", "x:" + mLocation[0] + "|y:" + mLocation[1]);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, POPUP_TRIANGLE_WIDTH_HEIGHT);
+            if (mLocation[0] < 0) {
+                Log.d("test", "left:" + ((mWidth + POPUP_WINDOW_PADDING_LEFT_RIGHT) / 2 + mLocation[0]));
+                params.setMargins(
+                        (int) ((mWidth + POPUP_WINDOW_PADDING_LEFT_RIGHT - POPUP_TRIANGLE_WIDTH_HEIGHT) / 2 + mLocation[0])
+                        , 0, 0, 0);
+            } else if (mLocation[0] + mWidth + POPUP_WINDOW_PADDING_LEFT_RIGHT > mScreenWidth) {
+                Log.d("test", "screen width:" + mScreenWidth + "|left:" + ((mWidth + POPUP_WINDOW_PADDING_LEFT_RIGHT) / 2 - mScreenWidth));
+                params.setMargins(
+                        (int) ((mLocation[0] + mWidth + POPUP_WINDOW_PADDING_LEFT_RIGHT - mScreenWidth)
+                                + (mWidth + POPUP_WINDOW_PADDING_LEFT_RIGHT - POPUP_TRIANGLE_WIDTH_HEIGHT) / 2)
+                        , 0, 0, 0);
+            } else {
+                params.gravity = Gravity.CENTER;
+            }
+            triangle.setLayoutParams(params);
             popupWindow.showAtLocation(mAttachView, Gravity.NO_GRAVITY, mLocation[0], mLocation[1]);
         }
         return this;
